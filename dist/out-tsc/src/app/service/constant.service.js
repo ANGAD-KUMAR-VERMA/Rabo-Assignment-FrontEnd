c2cd2a06851ff129544f2d532f84a9f17226796f
ANGAD-KUMAR-VERMA <56833868+ANGAD-KUMAR-VERMA@users.noreply.github.com>
2020-03-31 12:26:13 +0530

    updated gitignore

--- dist/out-tsc/src/app/service/constant.service.js
+++
@@ -1,7 +0,0 @@
-export class ConstantService {
-    constructor() {
-        this.noOfRows = 4;
-        this.requiredHeaderArray = ["First name", "Sur Name", "Issue Count", "Date Of Birth"];
-    }
-}
-//# sourceMappingURL=constant.service.js.map
